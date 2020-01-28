69d539389e5125698cc69866bbf72f48a5384f4a
beshr alqadi <besher.alqade.h@gmail.com>
2020-01-28 17:40:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+def display_board()
+
 end
-display_board([" ", " ", " ", " ", "X", " ", " ", " ", " "])
